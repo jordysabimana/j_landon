6172676f1eeb4254956dfe45d077e319a63c9c49
jordy sabimana <jord.sab@gmail.com>
2023-11-26 13:21:28 +0100

    Updating boilerplate App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React hh
+          Learn React and upload to AWS
         </a>
       </header>
     </div>
